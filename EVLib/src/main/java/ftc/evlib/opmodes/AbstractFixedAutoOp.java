386b7dd84e713a88513e27f121d6f31d733f25e4
Terence Morrison <terence@acropolisepc.com>
2019-01-13 12:59:11 -0500

    Merge branch '27DEC2018_Update' of https://github.com/TmoGit/ftc_app-master into 27DEC2018_Update
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rover2018/AutoOp2019.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rover2018/RobotCfg2018.java
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rover2018/TeleOp2018.java

---
+++ EVLib/src/main/java/ftc/evlib/opmodes/AbstractFixedAutoOp.java
@@ -0,0 +1,46 @@
+package ftc.evlib.opmodes;
+
+import ftc.electronvolts.statemachine.StateMachine;
+import ftc.electronvolts.util.units.Time;
+import ftc.evlib.hardware.config.RobotCfg;
+
+/**
+ * This file was made by the electronVolts, FTC team 7393
+ * Date Created: 9/13/16
+ *
+ * extends AbstractOp and adds a 30 second timer and a state machine
+ *
+ * @see AbstractOp
+ * @see StateMachine
+ */
+public abstract class AbstractFixedAutoOp<Type extends RobotCfg> extends AbstractOp<Type> {
+    protected StateMachine stateMachine;
+
+    /**
+     * This is implemented by the autonomous opmode
+     * It is called where the setup would have been
+     *
+     * @return A state machine to be run
+     */
+   // public abstract StateMachine buildStates();
+
+    @Override
+    public Time getMatchTime() {
+        return Time.fromSeconds(30); //autonomous is 30 seconds
+    }
+
+    @Override
+    public void setup() {
+      //  stateMachine = buildStates(); //get the state machine from the opmode
+    }
+
+    @Override
+    public void pre_act() {
+
+    }
+
+    @Override
+    public void post_act() {
+        stateMachine.act(); //update the state machine
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Rover2018/AutoOpTest2019.java
@@ -0,0 +1,132 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.Rover2018;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import ftc.electronvolts.util.InputExtractor;
+import ftc.electronvolts.util.Vector2D;
+import ftc.electronvolts.util.files.Logger;
+import ftc.evlib.hardware.control.RotationControls;
+import ftc.evlib.hardware.control.TranslationControls;
+import ftc.evlib.opmodes.AbstractFixedAutoOp;
+
+/**
+ * This file was made by Cut The Red Wire, FTC team 6078
+ */
+
+/**
+ * This file illustrates the concept of driving a path based on encoder counts.
+ * It uses the common Pushbot hardware class to define the drive on the robot.
+ * The code is structured as a LinearOpMode
+ *
+ * The code REQUIRES that you DO have encoders on the wheels,
+ *   otherwise you would use: PushbotAutoDriveByTime;
+ *
+ *  This code ALSO requires that the drive Motors have been configured such that a positive
+ *  power command moves them forwards, and causes the encoders to count UP.
+ *
+ *   The desired path in this example is:
+ *   - Drive forward for 48 inches
+ *   - Spin right for 12 Inches
+ *   - Drive Backwards for 24 inches
+ *   - Stop and close the claw.
+ *
+ *  The code is written using a method called: encoderDrive(speed, leftInches, rightInches, timeoutS)
+ *  that performs the actual movement.
+ *  This methods assumes that each movement is relative to the last stopping place.
+ *  There are other ways to perform encoder based moves, but this method is probably the simplest.
+ *  This code uses the RUN_TO_POSITION mode to enable the Motor controllers to generate the run profile
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="AutoOp2019", group="Official")
+
+public class AutoOpTest2019 extends AbstractFixedAutoOp<RobotCfg2018>  {
+
+    /* Declare OpMode members. */
+    @Override
+    protected RobotCfg2018 createRobotCfg() {
+        RobotCfg2018 robot  = new RobotCfg2018(hardwareMap);
+        return robot;
+    }
+
+    private ElapsedTime     runtime = new ElapsedTime();
+
+
+    static final double     COUNTS_PER_MOTOR_REV    = 288 ;    // eg: TETRIX Motor Encoder
+    static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+                                                      (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     MOTOR_RUNSPEED            = 0.75;
+    static final double     TURN_SPEED              = 0.5;
+
+    double gxVal = 0;
+    double gyVal = 0;
+    double grVal = 0;
+
+    Vector2D XYVector;
+
+    @Override
+    protected void setup_act() {
+
+    }
+
+    @Override
+    protected void end() {
+
+    }
+
+    @Override
+    protected void act() {
+
+    }
+
+    @Override
+    protected void go() {
+
+    }
+
+    @Override
+    protected Logger createLogger() {
+        return null;
+    }
+
+    }
+
+
+
+
